e01a0fd578fc6aa686817d6653896923e561a1ff
dgliang <2433353370@qq.com>
2024-11-24 12:59:57 +0800

    Complete CollectWork class

--- app/src/main/java/com/example/theychat/work/CollectWork.java
+++ app/src/main/java/com/example/theychat/work/CollectWork.java
@@ -1,19 +1,38 @@
 package com.example.theychat.work;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class CollectWork extends Worker {
+    private final static String TAG = "CollectWork";
+    private Data input;
+
     public CollectWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        input = workerParams.getInputData();
     }
 
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        String desc = String.format("请求参数包括：姓名=%s，身高=%d，体重=%f",
+                input.getString("name"),
+                input.getInt("height", 0),
+                input.getDouble("weight", 0)
+        );
+
+        Log.d(TAG, "doWork: " + desc);
+
+        Data output = new Data.Builder()
+                .putInt("resultCode", 0)
+                .putString("resultDesc", "处理成功")
+                .build();
+
+        return Result.success(output);
     }
 }
