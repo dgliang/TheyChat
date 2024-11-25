3f4c47b7181734fd0e299d9fde11415f371d2111
dgliang <2433353370@qq.com>
2024-11-25 11:43:30 +0800

    Add Okhttp dependency besides with some interface but not complete yet

--- app/src/main/java/com/example/theychat/ThreadUiActivity.java
+++ app/src/main/java/com/example/theychat/ThreadUiActivity.java
@@ -26,28 +26,37 @@ public class ThreadUiActivity extends AppCompatActivity {
 
         tv_message = findViewById(R.id.tv_message);
 
+        // 设置“开始播放”按钮点击监听
         findViewById(R.id.btn_start).setOnClickListener(view -> {
+            // 如果当前没有在播放新闻，启动新闻播放线程
             if (!isPlaying) {
                 isPlaying = true;
                 new Thread(this::broadcastNews).start();
             }
         });
+
+        // 设置“停止播放”按钮点击监听
         findViewById(R.id.btn_stop).setOnClickListener(view -> isPlaying = false);
     }
 
     private void broadcastNews() {
         String startDesc = String.format("%s\n%s %s", tv_message.getText().toString(), DateUtil.getNowTime(), "开始播放新闻");
         runOnUiThread(() -> tv_message.setText(startDesc));
+
+        // 循环播放新闻，直到停止
         while (isPlaying) {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            // 随机选择一条新闻，并显示当前时间
             String runDesc = String.format("%s\n%s %s", tv_message.getText().toString(), DateUtil.getNowTime(),
                     newsArray[new Random().nextInt(5)]);
             runOnUiThread(() -> tv_message.setText(runDesc));
         }
+
+        // 播放结束，显示结束信息
         String endDesc = String.format("%s\n%s %s", tv_message.getText().toString(), DateUtil.getNowTime(), "新闻播放结束，谢谢观看");
         runOnUiThread(() -> tv_message.setText(endDesc));
         isPlaying = false;
